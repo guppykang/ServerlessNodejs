f0391e60fed4399a62a1ab5655a9ec05af0b1d55
Joshua Kang <guppykang@gmail.com>
2019-08-29 01:06:15 -0700

    need to fix mongooose model

--- microservice/api/date.js
+++ microservice/api/date.js
@@ -1,8 +1,14 @@
 const moment = require('moment');
 const { mongoose, connection } = require('../mongoose_connections');
 
-module.exports = (req, res) => {
+const dateSchema = new mongoose.Schema({
+  current : String
+ });
+
+const date = connection.model("Date", dateSchema);
+
+module.exports = async (req, res) => {
   const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
-  let model = mongoose.model
-  res.end(currentTime);
+  const uploadResponse = await date.create({ current : currentTime });
+  res.end({ currentTime, uploadResponse });
 }
